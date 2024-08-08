062a4dae09280cdf75816fcd103b6a5fca06a631
FlexonaFFt <flexonafft@bk.ru>
2024-08-08 16:48:04 +0300

    --load simble version of database

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const Database = require("./database");
 
 const app = express();
 app.use(express.json());
 
-mongoose
-  .connect("mongodb://localhost:3000/mydatabase", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+const db = new Database("database.json");
 
-app.use("/api", routes);
+// Регистрация пользователя
+app.post("/register", (req, res) => {
+  const { email, nickname, password } = req.body;
+  db.insert({ email, nickname, password });
+  res.status(201).json({ message: "Пользователь зарегистрирован" });
+});
+
+// Создание записи
+app.post("/posts", (req, res) => {
+  const { userId, title, text } = req.body;
+  const user = db.find({ _id: userId })[0];
+  if (!user) {
+    return res.status(404).json({ message: "Пользователь не найден" });
+  }
+  user.posts = user.posts || [];
+  user.posts.push({ title, text });
+  db.save();
+  res.status(201).json({ message: "Запись создана" });
+});
+
+// Получение записей пользователя
+app.get("/posts/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const user = db.find({ _id: userId })[0];
+  if (!user || !user.posts) {
+    return res.status(404).json({ message: "Записи не найдены" });
+  }
+  res.json(user.posts);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
